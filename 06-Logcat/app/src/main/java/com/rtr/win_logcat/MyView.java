f21bff30c17e0bd82e611306c4f4c8d12d328c58
Rutwik Choughule <rutwik110@gmail.com>
2019-07-02 00:02:13 +0530

    Added logcat

---
+++ 06-Logcat/app/src/main/java/com/rtr/win_logcat/MyView.java
@@ -0,0 +1,98 @@
+package com.rtr.win_logcat;
+
+// added by me
+import androidx.appcompat.widget.AppCompatTextView;       // AppCompatTextView
+import android.content.Context;                           // Context
+import android.graphics.Color;                            // Color
+import android.view.Gravity;                              // Gravity
+import android.view.MotionEvent;                          // MotionEvent
+import android.view.GestureDetector;                      // GestureDetector
+import android.view.GestureDetector.OnGestureListener;    // OnGestureListener
+import android.view.GestureDetector.OnDoubleTapListener;  // OnDoubleTapListener
+
+public class MyView extends AppCompatTextView implements OnGestureListener, OnDoubleTapListener {
+
+    private GestureDetector gestureDetector;
+
+	public MyView(Context context) {
+		super(context);
+		setTextColor(Color.rgb(255, 128, 0));
+		setTextSize(60);
+		setGravity(Gravity.CENTER);
+		setText("Hello World!!!");
+
+        gestureDetector = new GestureDetector(context, this, null, false);
+        gestureDetector.setOnDoubleTapListener(this);
+	}
+
+    // handling 'onTouchEvent' is the most important
+    // because it triggers all gesture and tap events
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // code
+        int eventaction = event.getAction();  // not required for now
+        if (!gestureDetector.onTouchEvent(event))
+            super.onTouchEvent(event);
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapEventListener so must be implemented
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        setText("Double Tap");
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapListener so must be implemented
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        // do not write anything here, because already written 'onDoubleTap'
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapListener so must be implemented
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        setText("Single Tap");
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must implement
+    @Override
+    public boolean onDown(MotionEvent e) {
+        // do not write anything here, because already written in 'onSingleTapConfirmed'
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public void onLongPress(MotionEvent e) {
+        setText("Long Press");
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        System.exit(0);
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return(true);
+    }
+}
+
