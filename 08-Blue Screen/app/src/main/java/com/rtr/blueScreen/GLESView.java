128715dc731b45373927a2ed83698cae224182d2
Rutwik Choughule <rutwik110@gmail.com>
2019-08-12 02:04:18 +0530

    Added Blue Screen

---
+++ 08-Blue Screen/app/src/main/java/com/rtr/blueScreen/GLESView.java
@@ -0,0 +1,140 @@
+package com.rtr.blueScreen;
+
+// added by me
+import android.opengl.GLSurfaceView;              // SurfaceView with support of OpenGL
+import android.opengl.GLES32;                     // OpenGL ES 3.2
+import javax.microedition.khronos.opengles.GL10;  // OpenGL Extension for basic features of OpenGL ES
+import javax.microedition.khronos.egl.EGLConfig;  // Embedded Graphics Library
+
+import android.content.Context;                           // Context
+import android.view.MotionEvent;                          // MotionEvent
+import android.view.GestureDetector;                      // GestureDetector
+import android.view.GestureDetector.OnGestureListener;    // OnGestureListener
+import android.view.GestureDetector.OnDoubleTapListener;  // OnDoubleTapListener
+
+public class GLESView extends GLSurfaceView implements GLSurfaceView.Renderer, OnGestureListener, OnDoubleTapListener {
+
+    private final Context context;
+    private GestureDetector gestureDetector;
+
+	public GLESView(Context drawingContext) {
+		super(drawingContext);
+		context = drawingContext;
+
+        setEGLContextClientVersion(3);   // highest supported 3.x
+        setRenderer(this);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY); 
+
+        gestureDetector = new GestureDetector(context, this, null, false);
+        gestureDetector.setOnDoubleTapListener(this);
+	}
+
+    // handling 'onTouchEvent' is the most important
+    // because it triggers all gesture and tap events
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // code
+        int eventaction = event.getAction();  // not required for now
+        if (!gestureDetector.onTouchEvent(event))
+            super.onTouchEvent(event);
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapEventListener so must be implemented
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapListener so must be implemented
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        // do not write anything here, because already written 'onDoubleTap'
+        return(true);
+    }
+
+    // abstract method from OnDoubleTapListener so must be implemented
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must implement
+    @Override
+    public boolean onDown(MotionEvent e) {
+        // do not write anything here, because already written in 'onSingleTapConfirmed'
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public void onLongPress(MotionEvent e) {
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        System.exit(0);
+        return(true);
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    // abstract method from OnGestureListener so must be implemented
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return(true);
+    }
+
+    ///// implementation of GLSurfaceView.Renderer methods
+    @Override
+    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        String version = gl.glGetString(GL10.GL_VERSION);
+        System.out.println("RTR: " + version);
+
+        initialize();
+    }
+
+    @Override
+    public void onSurfaceChanged(GL10 unused, int width, int height) {
+        resize(width, height);
+    }
+
+    @Override
+    public void onDrawFrame(GL10 unused) {
+        display();
+    }
+
+    ///// our custom methods
+    private void initialize() {
+        GLES32.glClearColor(0.0f, 0.0f, 1.0f, 0.0f);
+    }
+
+    private void resize(int width, int height) {
+        GLES32.glViewport(0, 0, width, height);
+    }
+
+    private void display() {
+        GLES32.glClear(GLES32.GL_COLOR_BUFFER_BIT | GLES32.GL_DEPTH_BUFFER_BIT);
+        requestRender();  // ~ swapBuffers
+    }
+}
+
+
+
+
+
+
+
+
+
