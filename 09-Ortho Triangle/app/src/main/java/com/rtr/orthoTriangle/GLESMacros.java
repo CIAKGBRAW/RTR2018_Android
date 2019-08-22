74932ebfb60e1226efc3c2cf62bb3a66ff3dea5e
Rutwik Choughule <rutwik110@gmail.com>
2019-08-23 00:55:30 +0530

    Fixed macros

--- 09-Ortho Triangle/app/src/main/java/com/rtr/orthoTriangle/GLESMacros.java
+++ 09-Ortho Triangle/app/src/main/java/com/rtr/orthoTriangle/GLESMacros.java
@@ -4,7 +4,7 @@ package com.rtr.orthoTriangle;
 public class GLESMacros {
 
     public static final int AMC_ATTRIBUTE_POSITION  = 0;
-    public static final int AMC_ATTRIBUTE_COLOR     = 0;
-    public static final int AMC_ATTRIBUTE_NORMAL    = 0;
-    public static final int AMC_ATTRIBUTE_TEXCOORD0 = 0;
+    public static final int AMC_ATTRIBUTE_COLOR     = 1;
+    public static final int AMC_ATTRIBUTE_NORMAL    = 2;
+    public static final int AMC_ATTRIBUTE_TEXCOORD0 = 3;
 }
